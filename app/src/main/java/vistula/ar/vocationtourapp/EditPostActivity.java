695fba69e6ea8b9c42c5b9348b304c4b0e224e35
rahmangithub2 <rahman.github2@gmail.com>
2022-03-27 08:33:20 +0200

    forth

---
+++ app/src/main/java/vistula/ar/vocationtourapp/EditPostActivity.java
@@ -0,0 +1,13 @@
+package vistula.ar.vocationtourapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_post);
+    }
+}
--- app/src/main/java/vistula/ar/vocationtourapp/HomeActivity.java
+++ app/src/main/java/vistula/ar/vocationtourapp/HomeActivity.java
@@ -1,14 +1,83 @@
 package vistula.ar.vocationtourapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import vistula.ar.vocationtourapp.adapter.PostsAdapter;
+import vistula.ar.vocationtourapp.fragments.HomeFragment;
+import vistula.ar.vocationtourapp.model.Post;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private FloatingActionButton fab;
+    private BottomNavigationView navigationView;
+    private static final int GALLERY_ADD_POST = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frameHomeContainer,new HomeFragment(),HomeFragment.class.getSimpleName()).commit();
+        init();
     }
+
+    private void init(){
+        navigationView = findViewById(R.id.bottom_nav);
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(v->{
+            Intent i = new Intent(Intent.ACTION_PICK);
+            i.setType("image/*");
+            startActivityForResult(i,GALLERY_ADD_POST);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode==GALLERY_ADD_POST && resultCode==RESULT_OK){
+//            Uri imgUri = data.getData();
+//            Intent i = new Intent(HomeActivity.this,AddPostActivity.class);
+//            i.setData(imgUri);
+//            startActivity(i);
+//        }
+    }
+
+
 }
